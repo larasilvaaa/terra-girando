5ced155a73f9b73bf3ef019462cb9a96ac37560b
larasilvaaa <lara.ramos.silva@escola.pr.gov.br>
2025-07-29 11:45:10 -0300

    ff

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ function setup() {
 }
 
 function draw() {
-  background(150, 240, 93); //cor de fundo
+  background(300, 240, 93); //cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(100, 150); //esfera 3D
+  sphere(200, 200); //esfera 3D
   
   
 }
